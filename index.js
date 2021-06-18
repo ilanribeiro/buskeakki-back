0de89d5e5882249e5805f7cd71a53f613b14ff25
ilanribeiro <ilanribeiro@gmail.com>
2021-06-17 21:28:34 -0300

    Adiciona cors. Modifica rota para /produtos

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 const productRoute = require('./routes/productRoute');
 
 const port = process.env.PORT;
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => res.send("Hello World!"));
-app.use('/product', productRoute);
+app.use('/produtos', productRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
