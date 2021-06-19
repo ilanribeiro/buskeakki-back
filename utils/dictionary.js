62693064b5677fe0dc9436d94935bf277b9bab0a
ilanribeiro <ilanribeiro@gmail.com>
2021-06-19 18:05:22 -0300

    Adiciona dicionário com códigos de status http e mensagens de status.

---
+++ utils/dictionary.js
@@ -0,0 +1,21 @@
+const code200 = 200;
+const code201 = 201;
+const code204 = 204;
+const code400 = 400;
+const code404 = 404;
+const code500 = 500;
+
+const message = {
+  filterSaved: 'Filtro de busca salvo com sucesso',
+  internalError: 'Erro interno',
+};
+
+module.exports = {
+  code200,
+  code201,
+  code204,
+  code400,
+  code404,
+  code500,
+  message,
+};
