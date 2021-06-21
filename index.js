f23002c69c36420476da6e8b44e7764b0b0e34b0
ilanribeiro <ilanribeiro@gmail.com>
2021-06-20 23:28:29 -0300

    Adiciona parâmetro para limite de envio de dados via requisição post

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const productRoute = require('./routes/productRoute');
 const port = process.env.PORT;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit:'50mb' }));
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World!"));
